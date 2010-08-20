177da5fedcef7961255743e863d8f188439cada1
Christopher Sun <christophersun@Temple-of-SunDawg.local>
2010-08-20 09:09:53 -0400

    still fixing indentation

--- src/main/java/com/animoto/api/resource/BaseResource.java
+++ src/main/java/com/animoto/api/resource/BaseResource.java
@@ -28,12 +28,12 @@ public abstract class BaseResource implements Resource {
   protected String httpCallback;
   protected HttpCallbackFormat httpCallbackFormat = HttpCallbackFormat.XML;
   protected String state;
-	protected String requestId;
-	protected Map<String, String> links = new HashMap<String, String>();
-	protected Map<String, String> metadata = new HashMap<String, String>();
-	protected Storyboard storyboard;
-	protected Video video;
-	protected Response response; 
+  protected String requestId;
+  protected Map<String, String> links = new HashMap<String, String>();
+  protected Map<String, String> metadata = new HashMap<String, String>();
+  protected Storyboard storyboard;
+  protected Video video;
+  protected Response response; 
 
   public void setHttpCallback(String httpCallback) {
     this.httpCallback = httpCallback;
@@ -52,7 +52,7 @@ public abstract class BaseResource implements Resource {
   }
 
   public String getLocation() {
-		return links.get("self");
+    return links.get("self");
   }
 
   public void setState(String state) {
@@ -63,17 +63,17 @@ public abstract class BaseResource implements Resource {
     return state;
   }
 
-	public void setRequestId(String requestId) {
-		this.requestId = requestId;
-	}
+  public void setRequestId(String requestId) {
+    this.requestId = requestId;
+  }
 
-	public String getRequestId() {
-		return requestId;
-	}
+  public String getRequestId() {
+    return requestId;
+  }
 
-	public boolean isFailed() {
-		return "failed".equals(state);
-	}
+  public boolean isFailed() {
+    return "failed".equals(state);
+  }
 
   public boolean isPending() {
     return !("completed".equals(state));
@@ -83,21 +83,21 @@ public abstract class BaseResource implements Resource {
     return !isPending();
   }
 
-	public void setLinks(Map<String, String> links) {
-		this.links = links;
-	}
-	
-	public Map<String, String> getLinks() {
-		return links;
-	}
+  public void setLinks(Map<String, String> links) {
+    this.links = links;
+  }
+  
+  public Map<String, String> getLinks() {
+    return links;
+  }
 
-	public void setMetadata(Map<String, String> metadata) {
-		this.metadata = metadata;
-	}
+  public void setMetadata(Map<String, String> metadata) {
+    this.metadata = metadata;
+  }
 
-	public Map<String, String> getMetadata() {
-		return metadata;
-	}
+  public Map<String, String> getMetadata() {
+    return metadata;
+  }
 
   public void setStoryboard(Storyboard storyboard) {
     this.storyboard = storyboard;
@@ -115,61 +115,61 @@ public abstract class BaseResource implements Resource {
     return video;
   }
 
-	public Response getResponse() {
-		return response;
-	}
+  public Response getResponse() {
+    return response;
+  }
 
   protected Gson newGson() {
     return GsonUtil.create();
   }
 
-	public void handleHttpResponse(HttpResponse httpResponse, int expectedStatusCode) throws HttpExpectationException, IOException {
+  public void handleHttpResponse(HttpResponse httpResponse, int expectedStatusCode) throws HttpExpectationException, IOException {
     int statusCode;
     String body;
-		ApiResponse apiResponse;
-		BaseResource dtoBaseResource;
+    ApiResponse apiResponse;
+    BaseResource dtoBaseResource;
 
     statusCode = httpResponse.getStatusLine().getStatusCode();
     body = StringUtil.convertStreamToString(httpResponse.getEntity().getContent());
-		apiResponse = newGson().fromJson(body, ApiResponse.class);
+    apiResponse = newGson().fromJson(body, ApiResponse.class);
     if (statusCode != expectedStatusCode) {
       throw new HttpExpectationException(statusCode, expectedStatusCode, body, apiResponse.getResponse().getStatus());
     }
 
-		response = apiResponse.getResponse();
+    response = apiResponse.getResponse();
     dtoBaseResource = apiResponse.getResponse().getPayload().getBaseResource(this.getClass());
-		
+    
     doErrorableBeanCopy(dtoBaseResource);
     setRequestId(httpResponse.getFirstHeader("x-animoto-request-id").getValue());
 
     if (getLocation() == null) {
       throw new ContractError();
     }
-	}
+  }
 
-	protected void populateStoryboard() {
+  protected void populateStoryboard() {
     if (isComplete()) {
       Storyboard storyboard = new Storyboard();
       storyboard.getLinks().put("self", getLinks().get("storyboard"));
       setStoryboard(storyboard);
-			if (storyboard.getLocation() == null) {
-				throw new ContractError();
-			}
+      if (storyboard.getLocation() == null) {
+        throw new ContractError();
+      }
     }
-	}
+  }
 
-	protected void populateVideo() {
+  protected void populateVideo() {
     if (isComplete()) {
       Video video = new Video();
       video.getLinks().put("self", getLinks().get("video"));
       setVideo(video);
-			if (video.getLocation() == null) {
-				throw new ContractError();
-			}
+      if (video.getLocation() == null) {
+        throw new ContractError();
+      }
     }
-	}
+  }
 
-	protected void doErrorableBeanCopy(Object bean) {
+  protected void doErrorableBeanCopy(Object bean) {
     try {
       BeanUtils.copyProperties(this, bean);
     }
@@ -182,5 +182,5 @@ public abstract class BaseResource implements Resource {
     catch (InvocationTargetException e) {
       throw new Error();
     }
-	}
+  }
 }
