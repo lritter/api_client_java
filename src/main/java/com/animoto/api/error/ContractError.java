0c36c7bb02834270342619150bc3e91c6895cd12
SunDawg <christopher.sun@yahoo.com>
2010-08-20 13:42:31 -0400

    blah

--- src/main/java/com/animoto/api/error/ContractError.java
+++ src/main/java/com/animoto/api/error/ContractError.java
@@ -1,7 +1,7 @@
 package com.animoto.api.error;
 
 /**
- * A ContractError is thrown when the API response message for a given resource does not adhere to the current RESTFUL requirements.<p/>
+ * A ContractError is thrown when the API response message for a given resource does not adhere to the current service contract requirements as specified in the documentation.<p/>
  *
  * For example, when an API response does not contain Storyboard when the DirectingJob has successfully completed or the creation of a RenderingJob does not contain a link to self from which we can reload resource information to query status.<p/>
  *
