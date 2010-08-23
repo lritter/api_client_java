fe26ff5161a0a4bf5fb6454cd8fc641bbd72eed2
SunDawg <christopher.sun@yahoo.com>
2010-08-23 17:34:39 -0400

    added wire logging help to the scripts

--- cli.sh
+++ cli.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -cp target/animoto-api-1.0-SNAPSHOT-jar-with-dependencies.jar com.animoto.api.cli.ApiClientCli $*
+java -Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog -Dorg.apache.commons.logging.simplelog.log.org.apache.http=DEBUG -cp target/animoto-api-1.0-SNAPSHOT-jar-with-dependencies.jar com.animoto.api.cli.ApiClientCli $*
